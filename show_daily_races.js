3c4617f6549ba0b5127b7a3ae0d7620c37799980
rickstaa <rick.staa@outlook.com>
2023-08-21 19:00:38 +0200

    chore: release v1.1.0

--- show_daily_races.js
+++ show_daily_races.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Nitro Type - Show Daily Races
 // @namespace    https://github.com/rickstaa/nitro-type-show-daily-races
-// @version      1.0.1
+// @version      1.1.0
 // @description  Displays the number of daily races completed by each team member in the team roster table on the Nitro Type team page.
 // @author       Rick Staa
 // @match        *://*.nitrotype.com/team/*
