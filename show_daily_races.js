21c80abf08fe024e122045335e2432479c0d5f53
rickstaa <rick.staa@outlook.com>
2023-08-21 10:39:37 +0200

    fix: improves the daily races average
    This commit improves the average for members that joined more than a
    month ago. For these members it now uses the official
    [NitroType api](https://www.nitrotype.com/api/v2/teams/) to retrieve
    the memberDays.

--- show_daily_races.js
+++ show_daily_races.js
@@ -10,74 +10,49 @@
 // @license      MIT
 // ==/UserScript==
 
+const TEAM_ID = window.location.href.split("/")[4];
+let TEAM_STATS = null;
+
+/**
+ * Fetches team stats from NitroType API.
+ * @param {number} teamId - The ID of the team to fetch stats for.
+ * @returns {Promise<Object>} - A promise that resolves to an object containing the team stats.
+ */
+const fetchTeamStats = async (teamId) => {
+  const response = await fetch(
+    `https://www.nitrotype.com/api/v2/teams/${teamId}`
+  );
+  const data = await response.json();
+  return data;
+};
+
+/**
+ * Wait for the element to be available in the DOM.
+ * @param {*} selector The selector to wait for.
+ * @returns The element.
+ */
+function waitForElm(selector) {
+  return new Promise((resolve) => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector));
+    }
+
+    const observer = new MutationObserver((_) => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector));
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  });
+}
+
 (function () {
   "use strict";
-
-  /**
-   * Returns the number of days based on the given time string.
-   * @param {string} timeString - The time string to parse.
-   * @returns {number} The number of days.
-   */
-  const getTimeInDays = (timeString) => {
-    let timeComponents = timeString.split(" ");
-
-    // Retrieve the number of days on the team.
-    const counter = timeComponents[0] != "a" ? timeComponents[0] : 1;
-    const timeKey = timeComponents[1] ? timeComponents[1] : "day";
-    let days;
-    switch (timeKey) {
-      case "year":
-        days = 365;
-        break;
-      case "years":
-        days = 365 * counter;
-        break;
-      case "month":
-        days = 30;
-        break;
-      case "months":
-        days = 30 * counter;
-        break;
-      case "week":
-        days = 7;
-        break;
-      case "weeks":
-        days = 7 * counter;
-        break;
-      case "days":
-        days = 1 * counter;
-        break;
-      default:
-        days = 1;
-        break;
-    }
-    return days;
-  };
-
-  /**
-   * Wait for the element to be available in the DOM.
-   * @param {*} selector The selector to wait for.
-   * @returns The element.
-   */
-  function waitForElm(selector) {
-    return new Promise((resolve) => {
-      if (document.querySelector(selector)) {
-        return resolve(document.querySelector(selector));
-      }
-
-      const observer = new MutationObserver((_) => {
-        if (document.querySelector(selector)) {
-          resolve(document.querySelector(selector));
-          observer.disconnect();
-        }
-      });
-
-      observer.observe(document.body, {
-        childList: true,
-        subtree: true,
-      });
-    });
-  }
 
   /**
    * Add the daily races column to the team stats table when the page has loaded.
@@ -92,6 +67,9 @@
     const teamStatsTableBody = await waitForElm(
       ".table.table--striped.table--selectable.table--team.table--teamOverview tbody"
     );
+    const teamStatsTableRows = Array.from(
+      teamStatsTable.querySelectorAll("tbody tr")
+    );
 
     // Add extra daily races header column.
     const dailyRacesHeader = document.createElement("th");
@@ -104,21 +82,42 @@
     teamStatsTableHeaderRow.appendChild(dailyRacesHeader);
 
     // Find Team Races and Members Since columns.
-    let teamRacesColumn = Array.from(teamStatsTableHeaderRow.cells).find((cell) =>
-      cell.innerHTML.includes("Team<br>Races")
+    let teamRacesColumn = Array.from(teamStatsTableHeaderRow.cells).find(
+      (cell) => cell.innerHTML.includes("Team<br>Races")
     ).cellIndex;
     let memberSinceColumn = Array.from(teamStatsTableHeaderRow.cells).find(
       (cell) => cell.innerHTML.includes("Member<br>Since")
     ).cellIndex;
 
     // Loop through all team members and display the daily races.
-    Array.from(teamStatsTable.querySelectorAll("tbody tr")).map((row) => {
+    for (let i = 0; i < teamStatsTableRows.length; i++) {
+      const row = teamStatsTableRows[i];
+
+      // Retrieve member days.
+      const memberSince = row.cells[memberSinceColumn].textContent.trim();
+      const timeComponents = memberSince.split(" ");
+      let memberDays = 0;
+      if (timeComponents[1] === "days") {
+        memberDays = parseInt(timeComponents[0], 10);
+      } else if (timeComponents[1] === "day") {
+        memberDays = 1;
+      } else {
+        if (!TEAM_STATS) {
+          TEAM_STATS = await fetchTeamStats(TEAM_ID);
+        }
+
+        // Retrieve number of days since the user joined the team.
+        const joinStamp = TEAM_STATS.results.members[i].joinStamp;
+        const jointDate = new Date(joinStamp * 1000);
+        const today = new Date();
+        const timeDiff = Math.abs(today.getTime() - jointDate.getTime());
+        memberDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+      }
+
       // Calculate daily races.
-      const memberDays = getTimeInDays(
-        row.cells[memberSinceColumn].textContent.trim()
-      );
       const races = parseInt(
-        row.cells[teamRacesColumn].textContent.replace(",", "")
+        row.cells[teamRacesColumn].textContent.trim().replace(",", ""),
+        10
       );
       const dailyRaces = races / memberDays;
 
@@ -127,26 +126,26 @@
       dailyRacesCell.classList.add("table-cell", "table-cell--lastRace");
       dailyRacesCell.textContent = dailyRaces.toFixed(0);
       row.appendChild(dailyRacesCell);
-    });
+    }
 
     // Add a click handler that sorts the team members by daily races.
     dailyRacesHeader.addEventListener("click", function () {
-      const rows = Array.from(teamStatsTable.querySelectorAll("tbody tr"));
-
       // Sort rows by dailyRaces in descending order.
-      rows.sort(function (a, b) {
+      teamStatsTableRows.sort(function (a, b) {
         const aDailyRaces = parseInt(
-          a.cells[teamStatsTableHeaderRow.cells.length - 1].textContent
+          a.cells[teamStatsTableHeaderRow.cells.length - 1].textContent.trim(),
+          10
         );
         const bDailyRaces = parseInt(
-          b.cells[teamStatsTableHeaderRow.cells.length - 1].textContent
+          b.cells[teamStatsTableHeaderRow.cells.length - 1].textContent.trim(),
+          10
         );
         return bDailyRaces - aDailyRaces;
       });
 
       // If already sorted descending change to ascending.
       if (dailyRacesHeader.classList.contains("table-filter--desc")) {
-        rows.reverse();
+        teamStatsTableRows.reverse();
         dailyRacesHeader.classList.remove("table-filter--desc");
         dailyRacesHeader.classList.add("table-filter--asc");
       } else {
@@ -155,20 +154,20 @@
 
       // Remove the sort icon from all other columns.
       Array.from(teamStatsTableHeaderRow.cells).forEach((cell) => {
-        if (cell != dailyRacesHeader) {
+        if (cell !== dailyRacesHeader) {
           cell.classList.remove("table-filter--asc", "table-filter--desc");
         }
       });
 
       // Remove rows and add them back in sorted order.
-      rows.forEach((row) => {
+      teamStatsTableRows.forEach((row) => {
         teamStatsTableBody.removeChild(row);
         teamStatsTableBody.appendChild(row);
       });
 
       // Remove the sort icon from daily races column when another column is clicked.
       Array.from(teamStatsTableHeaderRow.cells).forEach((cell) => {
-        if (cell != dailyRacesHeader) {
+        if (cell !== dailyRacesHeader) {
           cell.addEventListener("click", function () {
             dailyRacesHeader.classList.remove(
               "table-filter--asc",
